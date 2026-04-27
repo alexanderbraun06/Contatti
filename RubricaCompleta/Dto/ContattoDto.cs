227e3b5fec0f57975b46e48cd5a88ebf89021b90
Braun Alexander Maximilian Ralph <alexander.braun@itsincom.it>
2026-04-27 10:09:04 +0200

    Creazione dei DTO

---
+++ RubricaCompleta/Dto/ContattoDto.cs
@@ -0,0 +1,12 @@
+﻿namespace RubricaCompleta.Dto
+{
+    public class ContattoDto
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string? NumeroTelefonoAziendale { get; set; }
+        public string Email { get; set; }
+        public string IndirizzoPEC { get; set; }
+        public string Professione { get; set; }
+    }
+}
